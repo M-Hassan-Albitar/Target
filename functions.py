2ddee83b2f78d3ccb0a58d275c43b3797309fd4b
codermhassan <codermhassan@gmail.com>
2026-03-05 04:41:56 +0300

    update data

--- functions.py
+++ functions.py
@@ -14,10 +14,10 @@ def save_json(new_data):
 
 if __name__ == '__main__':
     new_data = {
-        "Branches": [{"id": 1, "Name": "Abha", "Target": "139,319", "Residual": "- 55,622", "Percentage": "- 40 %"},
-                     {"id": 2, "Name": "Albaha", "Target": "67,941", "Residual": "- 10,348", "Percentage": "- 15 %"},
-                     {"id": 3, "Name": "Jizan", "Target": "320,384", "Residual": "- 89,865", "Percentage": "- 28 %"},
-                     {"id": 4, "Name": "Khamis", "Target": "84,243", "Residual": "- 32,185", "Percentage": "- 38 %"},
-                     {"id": 5, "Name": "Najran", "Target": "132,361", "Residual": "- 31,221", "Percentage": "- 24 %"}]}
+        "Branches": [{"id": 1, "Name": "Abha", "Target": "139,319", "Residual": "- 51,616", "Percentage": "- 37 %"},
+                     {"id": 2, "Name": "Albaha", "Target": "67,941", "Residual": "- 7,213", "Percentage": "- 11 %"},
+                     {"id": 3, "Name": "Jizan", "Target": "320,384", "Residual": "- 78,842", "Percentage": "- 25 %"},
+                     {"id": 4, "Name": "Khamis", "Target": "84,243", "Residual": "- 26,673", "Percentage": "- 32 %"},
+                     {"id": 5, "Name": "Najran", "Target": "132,361", "Residual": "- 26,711", "Percentage": "- 20 %"}]}
 
     save_json(new_data)
