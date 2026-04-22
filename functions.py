98477b319a974f0dff4568428f56759289949905
codermhassan <codermhassan@gmail.com>
2026-04-22 12:39:35 +0300

    Update data

--- functions.py
+++ functions.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         "Branches": [{"id": 1, "Name": "Abha", "Target": "31,008", "Residual": "- 698", "Percentage": "- 2 %"},
                      {"id": 2, "Name": "Albaha", "Target": "18,837", "Residual": "- 3,470", "Percentage": "- 18 %"},
                      {"id": 3, "Name": "Jizan", "Target": "42,099", "Residual": "- 22,043", "Percentage": "- 52 %"},
-                     {"id": 4, "Name": "Khamis", "Target": "14,011", "Residual": "- 3,507", "Percentage": "+ 25 %"},
+                     {"id": 4, "Name": "Khamis", "Target": "14,011", "Residual": "+ 3,507", "Percentage": "+ 25 %"},
                      {"id": 5, "Name": "Najran", "Target": "26,769", "Residual": "- 11,399", "Percentage": "- 43 %"}]}
 
     save_json(new_data)
