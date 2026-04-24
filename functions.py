59a6c228ed3851ac609ed1b8de839f35eeba1602
codermhassan <codermhassan@gmail.com>
2026-04-24 14:33:26 +0300

    Update data

--- functions.py
+++ functions.py
@@ -14,10 +14,10 @@ def save_json(new_data):
 
 if __name__ == '__main__':
     new_data = {
-        "Branches": [{"id": 1, "Name": "Abha", "Target": "31,008", "Residual": "+ 1906", "Percentage": "+ 6 %"},
+        "Branches": [{"id": 1, "Name": "Abha", "Target": "31,008", "Residual": "+ 2340", "Percentage": "+ 8 %"},
                      {"id": 2, "Name": "Albaha", "Target": "18,837", "Residual": "- 2,168", "Percentage": "- 12 %"},
-                     {"id": 3, "Name": "Jizan", "Target": "42,099", "Residual": "- 18,484", "Percentage": "- 44 %"},
-                     {"id": 4, "Name": "Khamis", "Target": "14,011", "Residual": "+ 5,909", "Percentage": "+ 42 %"},
+                     {"id": 3, "Name": "Jizan", "Target": "42,099", "Residual": "- 14,259", "Percentage": "- 34 %"},
+                     {"id": 4, "Name": "Khamis", "Target": "14,011", "Residual": "+ 6,343", "Percentage": "+ 45 %"},
                      {"id": 5, "Name": "Najran", "Target": "26,769", "Residual": "- 11,399", "Percentage": "- 43 %"}]}
 
     save_json(new_data)
