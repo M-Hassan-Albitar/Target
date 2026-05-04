ae3601d0ff9ee5149b110e88baede113f5759b25
codermhassan <codermhassan@gmail.com>
2026-05-04 16:38:15 +0300

    Clear the sidebar

--- main.py
+++ main.py
@@ -196,16 +196,6 @@ else:
             )
 
             show_chart(old_df_name=df, branch_name="NAJRAN")
-    if st.session_state.get('authentication_status') == False or st.session_state.get(
-            'authentication_status') is None:
-        st.markdown(
-            """
-            <style>
-                [data-testid="stSidebar"] { display: none; }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
 
     # Logout button
     if st.button("تسجيل الخروج"):
